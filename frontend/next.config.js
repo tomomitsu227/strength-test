20d9641b1d5c204369a7bb4aa101f4810d5a54a3
tomomitsu227 <56288963+tomomitsu227@users.noreply.github.com>
2025-10-21 17:29:11 +0900

    Fix env var typo

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
-// next.config.js
-module.exports = {
   env: {
     NEXT_PUBLIC_API_BASE: process.env.NEXT_PUBLIC_API_BASE,
   },
-};
+}
+module.exports = nextConfig
