fb93ff0d00bd990e7c5b03b576a6b1aefb651cb6
tomomitsu227 <56288963+tomomitsu227@users.noreply.github.com>
2025-10-21 17:36:05 +0900

    Fix env var typo

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -18,7 +18,7 @@ import {
 
 ChartJS.register(RadialLinearScale, PointElement, LineElement, Filler, Tooltip, Legend);
 
-const APIBASE = process.env.NEXT_PUBLIC_API_BASE || "http://localhost:5000/api";
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE || "http://localhost:5000/api";
 
 
 interface Question {
